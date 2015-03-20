14fcc069a8a2a98a5055f009cc63119a462a6179
Daniel Neighman <has.sox@gmail.com>
2015-03-20 20:29:49 +0000

    More timezone fun

--- lib/business_time/business_days.rb
+++ lib/business_time/business_days.rb
@@ -1,14 +1,13 @@
 require 'active_support/time'
 
 module BusinessTime
-  
+
   class BusinessDays
     def initialize(days)
       @days = days
     end
 
     def after(time = Time.now)
-      time = Time.zone ? Time.zone.parse(time.strftime('%Y-%m-%d %H:%M:%S %z')) : Time.parse(time.strftime('%Y-%m-%d %H:%M:%S %z'))
       days = @days
       while days > 0 || !Time.workday?(time)
         days -= 1 if Time.workday?(time)
@@ -16,12 +15,11 @@ module BusinessTime
       end
       time
     end
-    
+
     alias_method :from_now, :after
     alias_method :since, :after
-    
+
     def before(time = Time.now)
-      time = Time.zone ? Time.zone.parse(time.rfc822) : Time.parse(time.rfc822)
       days = @days
       while days > 0 || !Time.workday?(time)
         days -= 1 if Time.workday?(time)
@@ -29,10 +27,8 @@ module BusinessTime
       end
       time
     end
-    
+
     alias_method :ago, :before
     alias_method :until, :before
-  
-  end  
-  
+  end
 end
